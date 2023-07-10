1a27d12b419200059365d4f7396026a91937072b
kcbiradar <khajabiradar360@gmail.com>
2023-07-10 12:19:12 +0530

    templates folder add

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+import urllib.request
+
+import json
 
 def home(request):
-    return HttpResponse('<h1>Building a weather app</h1>')
+    if request.method == 'POST':
+        city = request.POST['city']
+        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=b603d23fb255dff62245d64867d20c64').read()
+
+        list_of_data = json.loads(source)
+        data = {
+            "country_code":str(list_of_data['sys']['country']),
+            "coordinate" : str(list_of_data['coord']['lon']) + ', ' + str(list_of_data['coord']['lat']),
+            "temp" : str(list_of_data['main']['temp']) + " Celius",
+            "pressure" : str(list_of_data['main']['pressure']),
+            "humidity" : str(list_of_data['main']['humidity']),
+            "main" : str(list_of_data['weather'][0]['main']),
+            "description" : str(list_of_data['weather'][0]['description']),
+            "icon" : list_of_data['weather'][0]['icon'],
+        }
+
+        print(data)
+    else :
+        data = {}
+    return render(request , "weather_app/index.html" , data)
